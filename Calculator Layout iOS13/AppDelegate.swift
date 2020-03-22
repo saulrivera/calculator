0af7fbd7a51d015209260762b5da7ab712793ed9
Saul Rivera <saulriveraal@icloud.com>
2020-03-22 11:59:23 -0600

    Auto Layout

--- Calculator Layout iOS13/AppDelegate.swift
+++ Calculator Layout iOS13/AppDelegate.swift
@@ -2,7 +2,7 @@
 //  AppDelegate.swift
 //  Calculator Layout iOS13
 //
-//  Created by Angela Yu on 01/07/2019.
+//  Created by Saul Rivera on 01/07/2019.
 //  Copyright © 2019 The App Brewery. All rights reserved.
 //
 
